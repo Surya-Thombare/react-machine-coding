7b30009c7ee589b61a53e0d50db8856d8c527c99
Suryakant Thombre <suryathombre000@gmail.com>
2023-12-03 23:47:15 +0530

    CodeSand box iframe  implimentd

--- app/react-machine-coding/todo/page.tsx
+++ app/react-machine-coding/todo/page.tsx
@@ -26,17 +26,20 @@ const Page = () => {
         </span>
       </div>
       <div className="flex flex-row gap-8">
-        <div className="">
-          <CodeMirror
-            value={Code}
-            height="700px"
-            width="600px"
-            theme={vscodeDark}
-          />
-        </div>
-        <div className="">
-          <Todo />
-        </div>
+        <div className="m-2"></div>
+        <iframe
+          src="https://codesandbox.io/embed/26gtlt?view=Editor+%2B+Preview&module=%2Fsrc%2Findex.js&hidenavigation=1"
+          // style={{width:100%; height: 500px; border:0; border-radius: 4px; overflow:hidden}}
+          style={{
+            width: "800px",
+            height: "500px",
+            borderRadius: "4px",
+            overflow: "hidden",
+          }}
+          title="GridLights"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
       </div>
     </div>
   );
